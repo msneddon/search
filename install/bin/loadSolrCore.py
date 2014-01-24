be092182d20ace82e4bc7dbe014756ce209c4b65
Keith Keller <kkeller@lbl.gov>
2014-01-24 15:55:38 -0800

    Script/lib to populate a solr core with tab-delimited input files

---
+++ install/bin/loadSolrCore.py
@@ -0,0 +1,32 @@
+import requests
+import sys
+
+def push_to_solr(headerfilepath,filepath,core):
+    solr_url = "http://localhost:7077/search/"+core+"/update?wt=json&separator=%09"
+
+    commit_url = "http://localhost:7077/search/admin/cores?wt=json&action=RELOAD&core=" + core
+
+    username = "admin"
+    password = "***REMOVED***"
+
+    solrheaderdata = open (headerfilepath, 'r')
+    csvdata = open(filepath, "r")
+
+    alldata = solrheaderdata.read() + csvdata.read()
+    postheaders = {'content-type':'application/csv; charset=utf-8'}
+
+#    req = requests.post(solr_url,data=sys.stdin,headers=postheaders)
+    req = requests.post(solr_url,data=alldata,headers=postheaders)
+    print req.text
+    req = requests.get(commit_url)
+    print req.text
+
+
+if __name__ == "__main__":
+
+    import argparse
+
+    headerfilepath = sys.argv[1]
+    filepath = sys.argv[2]
+    core = sys.argv[3]
+    push_to_solr(headerfilepath,filepath,core)
