5061386ad5fd9f904c43c9ca9b9ed7f90fe4477f
Matt Henderson <mlhenderson@20-c9-d0-46-db-f9.dhcp.lbl.gov>
2014-01-23 17:08:11 -0800

    updated deploy script with solr config install

--- deploy.py
+++ deploy.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
     # check for KBase environment variables, if not present use defaults    
 
     
+    # copy Tomcat config file for solr into the runtime
     if args.install_tomcat_config:    
         # copy tomcat config files
         tomcat_config_source_dir = os.path.abspath(os.path.join(os.getcwd(),"install/solr/tomcat"))
@@ -27,9 +28,33 @@ if __name__ == "__main__":
         os.chdir(tomcat_config_source_dir)
         shutil.copytree(os.path.join(tomcat_config_source_dir,"Catalina"), os.path.join(tomcat_config_target_dir,"Catalina"))
     
+    # copy solr cores, solr war, solr core config to deployment area
     if args.install_solr_config:
-        # create solr directory in deployment area, copy solr config file, copy solr core directories, copy solr war file
-        pass
+        solr_config_source_dir = os.path.abspath(os.path.join(os.getcwd(),"install/solr/config"))
+        solr_config_target_dir = os.path.join(os.environ["TARGET"], "services/search/solr")
+
+        print "Creating directory : " + str(solr_config_target_dir)
     
+        # create solr directory in deployment area
+        os.mkdir(solr_config_target_dir)
+        
+        # copy solr core directories and core config file
+        core_top_dir = os.path.join(solr_config_source_dir, "cores")
+        core_files = os.listdir(core_top_dir)
+        
+        for x in core_files:
+            if os.path.isdir(x):
+                print "Copying directory structure : " + os.path.join(core_top_dir, x) + " to " + os.path.join(solr_config_target_dir, x)
+                shutil.copytree(os.path.join(core_top_dir, x), os.path.join(solr_config_target_dir, x))
+            elif os.path.isfile(x):
+                print "Copying file : " + os.path.join(core_top_dir, x) + " to " + solr_config_target_dir
+                shutil.copy(os.path.join(core_top_dir, x), solr_config_target_dir)        
+        
+        # copy solr war file
+        solr_runtime_source_dir = os.path.abspath(os.path.join(os.getcwd(),"install/solr/config/runtime")
+        solr_runtime_target_dir = os.path.join(os.environ["TARGET"], "services/search/solr")
+        
+        print "Copying solr.war file to " + solr_runtime_target_dir
+        shutil.copy(os.path.join(solr_runtime_source_dir, "solr.war"), solr_runtime_target_dir)
     
     
