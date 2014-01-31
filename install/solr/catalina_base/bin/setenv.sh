7b5eb023be740e0f187d1be0368151d5dae4a802
Keith Keller <kkeller@lbl.gov>
2014-01-31 00:13:10 +0000

    Use macros (to be replaced when doing make deploy) to specify deployment directory (to help deploy team)

--- install/solr/catalina_base/bin/setenv.sh
+++ install/solr/catalina_base/bin/setenv.sh
@@ -1,7 +1,7 @@
 
-CATALINA_BASE=/kb/deployment/services/search/catalina_base
-CATALINA_HOME=/kb/runtime/tomcat
-JAVA_HOME=/kb/runtime/java
+CATALINA_BASE=$KB_TOP/services/search/catalina_base
+CATALINA_HOME=$KB_RUNTIME/tomcat
+JAVA_HOME=$KB_RUNTIME/java
 
 export CATALINA_BASE CATALINA_HOME JAVA_HOME
 
