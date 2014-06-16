f9973e1ee5e4177706e4192b6e82f21984c78b30
Keith Keller <kkeller@lbl.gov>
2014-06-16 20:01:43 +0000

    Cleaning up documentation, adding extra switches to make it easier to do loads to dev or prod

--- src/dataExtraction/wsLoad/README.cs
+++ src/dataExtraction/wsLoad/README.cs
@@ -1,15 +1,29 @@
 # very quick notes on getting CS objects into workspace
 
+##### dump central store into flat files
 # configure my.cnf file in cwd, example in csToFlatFiles/my.cnf
 # (keep on local storage, not ~ NFS, or nfs will croak and your run will die)
 # this gets full dump
 bash csToFlatFiles/dumpFeatureTables
 # for debugging, can do
-# limit=' LIMIT 5000 ' bash csToFlatFiles/dumpFeatureTables
+# limit=' LIMIT 50000 ' bash csToFlatFiles/dumpFeatureTables
 
+# one time only: get publication info from pubmed
+cut -f2 fids2pubs.tab | sort -u | python ~/dev_container/modules/search/src/dataExtraction/solrLoad/publications_to_solr.py
 
-# need to remind myself what switches there are
-# in theory we want switches to ignore or replace existing objects
-# and a switch to specify which ws instance to use
-# and a switch to specify which workspace to use
-python csFlatFiles_to_ws.py
+##### load flat files into workspace
+# switches:
+# --sorted-file-dir=. path to where above dump is (defaults to .)
+# --skip-last for handling a partial dump
+# --debug uses ***REMOVED*** against dev04.berkeley; no switch uses production
+# --wsname to specify name of workspace
+# --skip-existing to skip Genome objects that already exist (default is to overwrite)
+# (--skip-existing does not look at objects referenced in Genome object! (e.g., FeatureSet, ContigSet)
+
+# example for test run
+python search/src/dataExtraction/wsLoad/csFlatFiles_to_ws.py --debug --skip-last --wsname=JunkWS06062014
+
+# example for loading to production ws
+# use kbase-login to get credentials
+kbase-login kbasesearch
+python search/src/dataExtraction/wsLoad/csFlatFiles_to_ws.py --wsname=KBaseRichGenomesLoad  > out 2> err
