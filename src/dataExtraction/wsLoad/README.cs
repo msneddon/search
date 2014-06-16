64e4bb2eb94f07f091666dc0085ffaa5a3a226f9
Keith Keller <kkeller@lbl.gov>
2014-06-16 18:35:48 +0000

    Start of docs for loading CS genomes/features to workspace

---
+++ src/dataExtraction/wsLoad/README.cs
@@ -0,0 +1,10 @@
+#very quick notes
+
+# no switch yet to set LIMIT or not
+bash csToFlatFiles/dumpFeatureTables
+
+# need to remind myself what switches there are
+# in theory we want switches to ignore or replace existing objects
+# and a switch to specify which ws instance to use
+# and a switch to specify which workspace to use
+python csFlatFiles_to_ws.py
