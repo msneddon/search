24bf02cbb16181b6882309405d3c3e4e380813b8
Matt Henderson <mhenderson@lbl.gov>
2014-12-17 17:10:51 -0800

    deleting old delete and load bash scripts

--- src/dataExtraction/solrLoad/delete_core_data.sh
+++
@@ -1,9 +0,0 @@
-
-core=$1
-
-echo deleting data from core $core
-
-curl -q "http://localhost:7077/search/admin/cores?wt=json&action=RELOAD&core=$core"
-curl "http://localhost:7077/search/$core/update?stream.body=<delete><query>*:*</query></delete>"
-
-curl -q "http://localhost:7077/search/admin/cores?wt=json&action=RELOAD&core=$core"
