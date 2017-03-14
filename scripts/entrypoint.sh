d283ae01c87a3f3a8882aa7b31b8da92a215cfde
msneddon <mwsneddon@lbl.gov>
2017-03-14 11:07:08 -0700

    check that config files are present

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 if [ $# -eq 0 ] ; then
-  sh ./scripts/start_service.sh
-elif [ "${1}" = "start" ] ; then
+  if [ ! -f /kb/module/search/config/search_config.ini ]; then
+    echo "Config file: /kb/module/search/config/search_config.ini not found! Exiting."
+    exit 1;
+  fi
+  if [ ! -f /kb/module/search/config/uwsgi_search.ini ]; then
+    echo "Config file: /kb/module/search/config/uwsgi_search.ini not found! Exiting."
+    exit 1;
+  fi
+  if [ ! -f /kb/module/search/config/categoryInfo.json ]; then
+    echo "Config file: /kb/module/search/config/categoryInfo.json not found! Exiting."
+    exit 1;
+  fi
   sh ./scripts/start_service.sh
 elif [ "${1}" = "bash" ] ; then
   bash
